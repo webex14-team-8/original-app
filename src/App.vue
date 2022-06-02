3d70417728a5eeb61a7321673b6549a55f232778
watabehimemi <watabe1011@gmail.com>
2022-06-03 02:56:22 +0900

    投稿フォームだけ作りました

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link>|
+    <router-link to="/post">投稿</router-link>
   </nav>
   <router-view />
 </template>
---
+++ src/views/PostView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <textarea v-model="text" placeholder="何か書いてみよう"></textarea>
+    </div>
+    <div>
+      <button v-on:click="postTweet">投稿</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    }
+  },
+  methods: {
+    postTweet() {
+      alert("まだお待ちを")
+    },
+  },
+}
+</script>
+
+<style scoped></style>
